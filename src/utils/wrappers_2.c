c810117d5aaba4fc91c2b10b414d80b5fb29b03b
Clément VIDON <cvidon@student.42.fr>
2022-06-30 14:48:56 +0200

    Typo

--- src/utils/wrappers_2.c
+++ src/utils/wrappers_2.c
@@ -14,11 +14,9 @@
 
 void	ft_w_signal(t_data *data, int signum, t_sighandler handler)
 {
-	dprintf (2, "hello\n");
 	if (signal(signum, handler) == SIG_ERR)
 	{
 		data->status = errno;
-		dprintf (2, "ciao\n");
 		ft_putstr_fd("minishell: ", 2);
 		ft_putstr_fd("error: ", 2);
 		ft_putendl_fd(strerror(errno), 2);
