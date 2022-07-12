35ffd7346300fc285b6311899b4fa51f1b1dcba1
Clément VIDON <cvidon@student.42.fr>
2022-07-12 19:33:58 +0200

    fix status code

--- minishell_42/src/exec/exec_2.c
+++ minishell_42/src/exec/exec_2.c
@@ -14,9 +14,11 @@
 
 static void	ft_parent_2(t_data *data, t_dlist *cmd, int pid)
 {
+	dprintf (2, "a. %i\n", data->status);
 	if (!cmd->next)
 		if (waitpid(pid, &data->status, 0) == -1)
-			ft_perror(data, cmd, errno);
+			ft_perror(data, cmd, errno); // TODO errno sucks
+	dprintf (2, "b. %i\n", data->status);
 	if (WIFEXITED(data->status) == EXIT_FAILURE)
 	{
 		data->status = WEXITSTATUS(data->status);
@@ -74,13 +76,13 @@ static void	ft_child_2(t_data *data, t_dlist *cmd)
 	{
 		ft_check_fd(data, ((t_cmd *)cmd->content)->fd_out);
 		ft_w_dup2(data, ((t_cmd *)cmd->content)->fd_out,
-			((t_cmd *)cmd->content)->out);
+				((t_cmd *)cmd->content)->out);
 	}
 	else if (cmd->next)
 	{
 		ft_close (data, cmd, &((t_cmd *)cmd->content)->fd[0]);
 		ft_w_dup2(data, ((t_cmd *)cmd->content)->fd[1],
-			((t_cmd *)cmd->content)->out);
+				((t_cmd *)cmd->content)->out);
 		ft_close (data, cmd, &((t_cmd *)cmd->content)->fd[1]);
 	}
 	if (ft_is_builtin(cmd) && ft_fork_builtin(cmd))
@@ -92,8 +94,8 @@ static void	ft_child_2(t_data *data, t_dlist *cmd)
 	if (!ft_is_builtin(cmd) && !((t_cmd *)cmd->content)->prg)
 		ft_perror(data, cmd, 127);
 	if (!ft_is_builtin(cmd)
-		&& execve(((t_cmd *)cmd->content)->prg,
-			((t_cmd *)cmd->content)->cmd, data->envtab) == -1)
+			&& execve(((t_cmd *)cmd->content)->prg,
+				((t_cmd *)cmd->content)->cmd, data->envtab) == -1)
 		ft_perror(data, cmd, 126);
 }
 
@@ -110,12 +112,12 @@ void	ft_child(t_data *data, t_dlist *cmd)
 	{
 		ft_check_fd(data, ((t_cmd *)cmd->content)->fd_in);
 		ft_w_dup2(data, ((t_cmd *)cmd->content)->fd_in,
-			((t_cmd *)cmd->content)->in);
+				((t_cmd *)cmd->content)->in);
 	}
 	else if (cmd->prev)
 	{
 		ft_w_dup2(data, ((t_cmd *)cmd->prev->content)->fd[0],
-			((t_cmd *)cmd->content)->in);
+				((t_cmd *)cmd->content)->in);
 		ft_close (data, cmd, &((t_cmd *)cmd->prev->content)->fd[0]);
 	}
 	ft_child_2(data, cmd);
